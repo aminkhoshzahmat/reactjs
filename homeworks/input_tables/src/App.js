85e3b60dbc1fe4e3613d662b7fff766fbc657a53
Amin Khoshzahmat <khoshzahmat@bugloos.nl>
2022-06-03 14:54:46 +0430

    create table component part 1

--- homeworks/input_tables/src/App.js
+++ homeworks/input_tables/src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import InputText from "./components/forms/InputText";
+import InputText from "./Components/shared/forms/InputText";
+import {ErrorBoundariesHandler} from "./Handlers/ErrorBoundariesHandler";
+import UsersList from "./Components/UsersList";
 
 function App() {
-  return (
-        <InputText
-            mode={false}
-            value='Default text goes here'
-        />
-  );
+    return (
+        <div>
+        <ErrorBoundariesHandler>
+            <InputText mode={false} value='Default text goes here'/>
+            <div style={{borderBottom: '1px solid #eee', padding: '10px'}}></div>
+            <UsersList/>
+        </ErrorBoundariesHandler>
+        </div>
+    );
 }
 
 export default App;
--- homeworks/input_tables/src/Components/shared/forms/InputText.js
+++ homeworks/input_tables/src/Components/shared/forms/InputText.js
@@ -5,12 +5,17 @@ export default class InputText extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isEditMode: true || this.props.mode,
+            isEditMode: this.props.mode,
             isValid: true,
-            textValue: this.props.value || '',
+            textValue: this.props.value || 'hello',
         }
     }
 
+
+    componentDidMount() {
+        // to test error boundary
+        // throw new Error('whoops!')
+    }
 
     toggleEditMode() {
         this.setState({isEditMode: !this.state.isEditMode})
@@ -32,17 +37,27 @@ export default class InputText extends Component {
         return (
             <div>
                 <input
-                    style={{float: 'left', display: this.state.isEditMode ? 'block' : 'none'}}
+                    style={{
+                        width: '200px',
+                        display: this.state.isEditMode ? 'inline-block' : 'none'
+                    }}
                     // value={this.props.value}
                     // defaultValue={this.props.value}
                     onChange={this.getInputValue}
                     onKeyDown={this.commitInputChanges.bind(this)}
                 />
-                <span style={{display: this.state.isEditMode ? 'none' : 'inline-block'}}>
+                <span
+                    style={{
+                        width: '200px',
+                        display: this.state.isEditMode ? 'none' : 'inline-block'
+                    }}
+                    onClick={() => this.toggleEditMode()}
+                >
                     {this.state.textValue !== '' ? this.state.textValue : 'No text to show'}
                 </span>
-                <button onClick={() => this.toggleEditMode()}>
-                    {this.state.isEditMode ? 'Save' : 'Edit'}
+                <button style={{cursor: 'pointer', border: '0', fontSize:'20px'}}
+                   onClick={() => this.toggleEditMode()}>
+                    {this.state.isEditMode ? '💾' : '📝'}
                 </button>
             </div>
         );
